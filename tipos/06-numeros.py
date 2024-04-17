60dd373140df0b45091a2eecc5ff23ee99cf3f4e
unnomada <unnomadacode@gmail.com>
2024-04-17 17:45:55 +0200

    Dia 3

---
+++ tipos/06-numeros.py
@@ -0,0 +1,20 @@
+# Variales numéricas
+numero = 2  # entero o integer
+decimal = 1.2  # float
+imaginario = 2 + 2j  # j sería el número imaginario
+
+# Operaciones
+print(1 + 2)  # Suma
+print(1 - 3)  # Resta
+print(1 * 3)  # Multiplicación
+print(1 / 3)  # Divisón con decimales
+print(1 // 3)  # División sin decimales
+print(8 % 3)  # Módulo (Resto de la divisón)
+print(2 ** 2)  # Potencias
+
+# Remplazando el valor de una variable
+numero += 2  # Se le suma a la variable el número de la derecha
+numero -= 2  # Se le resta a la variable el número de la derecha
+numero *= 2  # Se multiplica la variable por el número de la derecha
+numero /= 2  # Se divide la variable entre el número de la derecha
+print("numero", numero)
---
+++ tipos/09-conversion-tipos.py
@@ -0,0 +1,11 @@
+# x = input("")
+# int() # Transforma cualquier dato en int
+# str() # Transforma cualquier dato en string
+# float() # Transforma cualquier dato en float
+# bool() # Transforma cualquier dato en boolean
+
+print(bool(""))
+print(bool("0"))
+print(bool(None))
+print(bool(" "))
+print(bool(0))
